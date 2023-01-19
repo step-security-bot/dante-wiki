e1f9121e861fa1ab39f8017c9c0fea3a6a8708e2
clecap <clemens.cap@uni-rostock.de>
2023-01-19 12:55:40 +0100

    Not a commit message

--- containers/lap/bin/generate.sh
+++ containers/lap/bin/generate.sh
@@ -2,26 +2,15 @@
 
 # generate a local docker image for the docker context in containers/${CONTAINER_NAME}/src
 
-CONTAINER_NAME=lap
+IMAGE_NAME=lap
 
 # get directory this shell is running in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-## clean up existing old stuff
 echo ""
-echo "CLEANING UP EXISTING CONTAINERS...this may cause error messages"
-echo ""
-
-# -t 0 as the LAP can be shot down quickly
-docker container stop ${CONTAINER_NAME} -t 0
-docker container rm   ${CONTAINER_NAME}
-echo ""
-echo "CLEAN!"
-
-echo ""
-echo "BUILDING container with name ${CONTAINER_NAME} from docker context at ${DIR}/../src"
+echo "BUILDING image with name ${IMAGE_NAME} from docker context at ${DIR}/../src"
 echo ""
 
-docker build -t ${CONTAINER_NAME} ${DIR}/../src
+docker build -t ${IMAGE_NAME} ${DIR}/../src
 
 echo " "; echo "DONE" ; echo " "
--- containers/lap/src/entrypoint.sh
+++ containers/lap/src/entrypoint.sh
@@ -1,60 +1,27 @@
 #!/bin/sh
 
+#!/bin/sh
 
+# generic entrypoint.sh of ssh docker, may be overwritten by subsequent docker layers
+# NOTE: This design decision is usefull for ssh, since ssh will not remain the only layer as it provides no service after all
 
-## 
-## Fix permissions
-##
-chmod 400 /etc/ssl/apache2/server.key
-chmod 444 /etc/ssl/apache2/server.pem
+# call the specific entrypoint of ssh docker
+source /ssh-entry.sh
 
+source /apache-php-fpm-entry.sh
 
-
-
-###
-### SSH: Portion we need when we want ssh access for container debugging
-###
-echo -n "** Generating our own, local ssh key..."
-ssh-keygen -A
-echo "DONE"
-
-echo -n "** Starting sshd in background..."
-# -e: sends output to standard error instead of syslog
-# -d: debug
-# -D: NO daemonization
-/usr/sbin/sshd
-echo "DONE with sshd"
-echo 
-##
-## END SSH
-##
-
-echo -n "** Generating our php.ini file..."
-rm -f /etc/php7/php-new.ini
-cat /etc/php7/php.ini /etc/php7/mediawiki-alpine-lamp-php.ini > /etc/php7/php-new.ini
-echo "DONE with generating php.ini file"
-
-##
-## PHP-FPM7: Start PHP-FPM7
-##
-#echo -n "** Starting php-fpm7 in background..."
-#/usr/sbin/php-fpm7 --php-ini /etc/php7/php-new.ini
-#echo "DONE with php-fpm7"
-#echo
-##
-## END PHP-FPM7
-##
-
-##
-## APACHE: Start apache  TODO: ev exec einbauen 
-##
-echo -n "** Starting apache..."
-/usr/sbin/httpd -D FOREGROUND 
-echo "DONE with apache"
-echo
-##
-## END Apache
-##
-
+echo "Sleeping for infinity to keep docker container alive..."
 sleep infinity
-  
+echo "Finished sleeping. This should not happen"
+
+
+
+
+
+
+
+
+
+
+
+
