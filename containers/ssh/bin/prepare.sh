d09694b0026a8ee6b77363b4a485393b3acccdb8
clecap <clemens.cap@uni-rostock.de>
2023-05-15 16:03:54 +0200

    Not a commit message

--- containers/ssh/bin/prepare.sh
+++ containers/ssh/bin/prepare.sh
@@ -9,9 +9,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/PARAMETERS.sh
 
 rm -f ${DIR}/../login-key ${DIR}/../login-key.pub ${DIR}/../src/login-key.pub
-ssh-keygen -f ${DIR}/../src/login-key
+ssh-keygen -f ${DIR}/../login-key
 
-chmod 0400 ${DIR}/../src/login-key
+chmod 0400 ${DIR}/../login-key
 
 # copy the PUBLIC key into the docker context area
 cp ${DIR}/../login-key.pub ${DIR}/../src
