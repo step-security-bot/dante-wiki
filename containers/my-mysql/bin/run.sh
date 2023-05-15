721cae30ae4d9989d970887ff0dcc99accd47aea
clecap <clemens.cap@uni-rostock.de>
2023-05-15 22:25:27 +0200

    Not a commit message

--- containers/my-mysql/bin/run.sh
+++ containers/my-mysql/bin/run.sh
@@ -122,6 +122,7 @@ echo -n "*** Creating DB container ${CONTAINER_NAME} and got id= "
 docker run -d --name ${CONTAINER_NAME}                      \
   --network ${NETWORK}                                      \
   -h ${HOST_NAME}                                           \
+  -p:${PORT}:22                                             \
   --env USERNAME=${USERNAME}                                \
   -e MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}"           \
   -e MYSQL_DUMP_USER="${MYSQL_DUMP_USER}"                   \
