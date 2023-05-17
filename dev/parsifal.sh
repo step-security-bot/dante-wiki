e4006233e9c435ecb730121a1dd03f9e9103646c
clecap <clemens.cap@uni-rostock.de>
2023-05-17 23:54:15 +0200

    Not a commit message

--- dev/parsifal.sh
+++ dev/parsifal.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# pull new stuff into container
-docker exec -it my-lap-container /bin/ash
-
-
-# and pull new stuff into volume
+# pull new stuff into reference volume on target machine
 ../volumes/full/spec/git-clone-dante-from-parsifal.sh
+
+
+docker cp ${SRC} ${TEMP}:/${MOUNT}/${VOLUME_PATH}
+printf "DONE cp\n"
+
+printf "*** Setting permissions on ${MOUNT} to 100.101 for apache.apache  ..."
+docker exec ${TEMP} /bin/ash -c "chown -R 100.100 ${MOUNT}"
+printf "DONE fixing permissions\n"
+
