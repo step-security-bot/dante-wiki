35efcce2befca8953d54345056a519b8216fa231
clecap <clemens.cap@uni-rostock.de>
2023-05-24 20:55:02 +0200

    Not a commit message

--- volumes/full/spec/git-push-to-parsifal.sh
+++ volumes/full/spec/git-push-to-parsifal.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+echo "-----------------------------------------\n\n"
+
+
 # pushes all the work done locally since the last 
 
 # get directory where this script resides wherever it is called from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WIKI=${DIR}/../content/wiki-dir
 
+echo "*** Adding files:"
 cd ${WIKI}/extensions/Parsifal
-git add .
+git add . --verbose
 
 # ncom is the non comment commit which I use in my system 
-echo "*** Commit"
-git ncom
+echo "\n*** Commit"
+git ncom --verbose
 echo ""
 
 
 echo "*** Push to upstream"
-git push -u
+git push -u --verbose
 echo ""
 
 
