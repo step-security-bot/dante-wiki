b9e465429421cdae44e0a4febcef94b6a667dd69
clecap <clemens.cap@uni-rostock.de>
2023-06-04 19:06:30 +0200

    Not a commit message

--- bin/cleanup-remote.sh
+++ bin/cleanup-remote.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+printf "*** Stopping lap and mysql containers and removing them - takes a while \n\n"
 
 docker stop my-lap-container
 docker rm my-lap-container
 docker stop my-mysql
 docker rm my-mysql
 
-
+printf "\n\nDONE"
 
